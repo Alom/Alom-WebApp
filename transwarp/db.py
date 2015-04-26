c57cf58a01a2bb6ac71cdb2ec25aa09fb266053e
Alom <aolan123@126.com>
2015-04-26 16:36:08 +0800

    Update db.py

--- transwarp/db.py
+++ transwarp/db.py
@@ -1,7 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 #db.py
 
-_author_ = "Ao, Lan"
-_time_ = "2015.04.10"
+import time, uuid, functools, threading, logging
+
+__author__ = "Ao, Lan"
+__date__ = "2015.04.10"
+
+class Dict(dict):
+	def __init__(self, name=(), values=(), **kw):
+		super(Dict, self).__init__(**kw)
+		for k, v in name, values:
+			self[k] = v
+			
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Dict' object has no attribute %s" %key)
+			
+	def __setattr__(self, key, value):
+		self[key] = value
+		
+def next_id(t=None):
+	if t is None:
+		t = time.time()
+	return '%015d%s000' %(int(t*1000),uuid.uuid4().hex)
+	
+def _profiling(start, sql=''):
+	t = time.time() - start
+	if t > 0.1:
+		logging.warning('[PROFILING] [DB] %s: %s' %(t, sql))
+	else:
+		logging.info('[PROFILING] [DB] %s: %s' %(t, sql))
+		
+class DBError(Exception):
+	pass
+
+class MultiColumnsError(DBError):
+	pass
+
+class _LasyConnection(object):
+	
+	def __init__(self):
+		self.connection = None
+		
+	def cursor(self):
+		if self.connection is None:
+			connection = engine.connect()
+			logging.info('Open connection <%s>...' %hex(id(connection)))
+			self.connection = connection
+		return self.connection.cursor()
+		
+	def commit(self):
+		self.connection.commit()
+		
+	def rollback(self):
+		self.connection.rollback()
+		
+	def cleanup(self):
+		if self.connection:
+			connection = self.connection
+			self.connection = None
+			logging.info
+			connection.close()
+	
+
+		
+		
 
 class _Engine(object):
 	def __init__(self, connect):
