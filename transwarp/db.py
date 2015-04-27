b7348f431c1c6b6ad7ecc8bb6e4e2c538cd727d0
Alom <aolan123@126.com>
2015-04-27 14:51:57 +0800

    finish the db without doctest

--- transwarp/db.py
+++ transwarp/db.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 #db.py
-
-import time, uuid, functools, threading, logging
-
 __author__ = "Ao, Lan"
 __date__ = "2015.04.10"
+
+'''
+Database operation module
+'''
+
+import time, uuid, functools, threading, logging
 
 class Dict(dict):
 	def __init__(self, name=(), values=(), **kw):
@@ -99,6 +102,23 @@ class _DbCtx(threading.local):
 
 _db_ctx = _DbCtx()
 
+def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
+	import mysql.connector
+	global engine
+	if engine is not None:
+		raise DBError('Engine is already initialized.')
+	params = dict(user=user, password=password, database=database, port=port)
+	defaults = dict(use_unicode=True, charset="utf8", collation='utf8_general_ci',autocommit=False)
+	for k,v in defaults.iteritems():
+		params[k] = kw.pop(k,v)
+	params.update(kw)
+	params['buffered'] = True
+	engine = _Engine(lambda: mysql.connector.connect(**params))
+	#test connection
+	logging.info('Init mysql engine <%s> ok.' %hex(id(engine)))
+	
+
+
 class _ConnectionCtx(object):
 	def __enter__(self):
 		global _db_ctx
@@ -117,11 +137,89 @@ def connection():
 	return _ConnectionCtx()
 
 def with_connection(func):
-	with connection():
+	@functools.wraps(func)
 	def wrapper(*args, **kw):
-		return func(*args, **kw)
+		with _ConnectionCtx():
+			return func(*args, **kw)
 	return wrapper
 
+class _TransactionCtx(object):
+	def __enter__(self):
+		global _db_ctx
+		self.should_close_conn = False
+		if not _db_ctx.is_init():
+			_db_ctx.init()
+			self.should_close_conn = True
+		_db_ctx.transactions = _db_ctx.transactions + 1
+		return self
+
+	def __exit__(self,exctype, excvalue, traceback):
+		global _db_ctx
+		_db_ctx.transactions = _db_ctx.transactions - 1
+		try:
+			if _db_ctx.transactions == 0:
+				if exctype == None:
+					self.commit()
+				else:
+					self.rollback()
+		finally:
+			if self.should_close_conn:
+				_db_ctx.cleanup()
+	
+	def commit(self):
+		global _db_ctx
+		logging.info('commit transaction...')
+		try:
+			_db_ctx.connection.commit()
+			logging.info('commit ok.')
+		except:
+			logging.warning('commit failed. try rollback...')
+			_db_ctx.connection.rollback()
+			logging.warning('rollback ok.')
+			raise
+
+	def rollback(self)
+		global _db_ctx
+		logging.warning('rollback transaction...')
+		_db_ctx.connection.rollback()
+		logging.info('rollback ok.')
+
+def transaction():
+	'''
+	Create a transaction object so can use with statement:
+	
+	with transaction():
+        pass
+	
+	>>> def update_profile(id, name, rollback):
+	...     u = dict(id=id, name=name, email='%s@test.org' % name, passwd=name, last_modified=time.time())
+	...     insert('user', **u)
+	...     r = update('update user set passwd=? where id=?', name.upper(), id)
+	...     if rollback:
+	...         raise StandardError('will cause rollback...')
+	>>> with transaction():
+	...     update_profile(900301, 'Python', False)
+	>>> select_one('select * from user where id=?', 900301).name
+	u'Python'
+	>>> with transaction():
+	...     update_profile(900302, 'Ruby', True)
+	Traceback (most recent call last):
+	   ...
+	StandardError: will cause rollback...
+	>>> select('select * from user where id=?', 900302)
+	[]
+	'''
+	return _TransactionCtx()
+
+def with_transaction(func):
+	@functools.wrap(func)
+	def wrapper(*args, **kw):
+		_start = time.time()
+		with _TransactionCtx():
+			return func(*args, **kw)
+		_profiling(_start)
+	return wrapper
+'''
 @with_connection
 def update(SQL, *args):
 	global _db_ctx
@@ -161,51 +259,73 @@ def select(SQL, *args):
 	finally:
 		cursor.close()
 	return result_set
-			
+'''
 
-class _TransactionCtx(object):
-	def __enter__(self):
-		global _db_ctx
-		self.should_close_conn = False
-		if not _db_ctx.is_init():
-			_db_ctx.init()
-			self.should_close_conn = True
-		_db_ctx.transactions = _db_ctx.transactions + 1
-		return self
+def _select(sql, first, *args):
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?','%s')
+	logging.info('SQL: %s, ARGS: %s'%(sql,args))
+	try:
+		cursor = _db_ctx.connection.cursor()
+		cursor.execute(sql, args)
+		if cursor.description:
+			names = [x[0] for x in cursor.description]
+		if first:
+			values = cursor.fetchone()
+			if not value:
+				return None
+			return Dict(names, values)
+		return [Dict(names, x) for x in cursor,fetchall()]
+	finally:
+		if cursor:
+			cursor.close()
 
-	def __exit__(self,exctype, excvalue, traceback):
-		global _db_ctx
-		_db_ctx.transactions = _db_ctx.transactions - 1
-		try:
-			if _db_ctx.transactions == 0:
-				if exctype == None:
-					self.commit()
-				else:
-					self.rollback()
-		finally:
-			if self.should_close_conn:
-				_db_ctx.cleanup()
+@with_connection
+def select_one(sql, *args):
+	return _select(sql, True, *args)
 	
-	def commit(self):
-		global _db_ctx
-		try:
+@with_connection
+def select_int(sql, *args):
+	d = _select(sql, True, *args)
+	if len(d) != 1:
+		raise MultiColumnsError('Expect only one column.')
+	return d.values()[0]
+	
+@with_connection
+def select(sql, *args):
+	return _select(sql, False, *args)
+	
+@with_connection
+def _update(sql, *args):
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?', '%s')
+	logging.info('SQL: %s, ARGS: %s' %(sql, args))
+	try:
+		cursor = db_ctx.connection.cursor()
+		cursor.execute(sql, args)
+		r = cursor.rowcount
+		if _db_ctx.transactions = 0:
+			logging.info('auto commit')
 			_db_ctx.connection.commit()
-		except:
-			_db_ctx.connection.rollback()
-			raise
+		return r
+	finally:
+		if cursor:
+			cursor.close()
+			
+def insert(table, **kw):
+	cols, args = zip(*kw.iteritems())
+	sql = 'insert into `%s` (%s) values (%s)' % (table, ','.join(['`%s`' % col for col in cols]), ','(['?' for i in range(len(cols))]))
+	return _update(sql, *args)
 
-	def rollback(self)
-		global _db_ctx
-		_db_ctx.connection.rollback()
-
-def transaction():
-	return _TransactionCtx()
-
-def with_transaction(func):
-	with transaction():
-	def wrapper(*args, **kw):
-		return func(*args, **kw)
-	return wrapper
-
-
-		
+def update(sql, *args):
+	return _update(sql, *args)
+	
+if __name__=='__main__':
+	logging.basicConfig(level=logging.DEBUG)
+	create_engine('www-data', 'www-data', 'test')
+	update('drop table if exist user')
+	update('create table user (id int primary key, name text, email text, passwd text, last_modified real)')
+	import doctest
+	doctest.testmod()
